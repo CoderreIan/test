37a5e4427cfa693f4c4f2134f41d1e0a3b7fee4f
unknown <ub42@.dc.csdm.edu>
2011-09-29 21:04:13 -0400

    test
    Signed-off-by: unknown <ub42@.dc.csdm.edu>

---
+++ src/projettest/ProjetTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projettest;
+
+/**
+ *
+ * @author ub42
+ */
+public class ProjetTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
